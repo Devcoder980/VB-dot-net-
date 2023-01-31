6ce76a7a2e41fe3540fdcab75503e8a40a602b89
░▒▓ Prabhudev Bind ▓▒░ <pbind4545@gmail.com>
2023-01-31 16:49:19 +0530

    Add project files.

---
+++ ShivaniHello/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'label1
+        '
+        Me.label1.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.label1.AutoSize = True
+        Me.label1.BackColor = System.Drawing.Color.FromArgb(CType(CType(121, Byte), Integer), CType(CType(232, Byte), Integer), CType(CType(121, Byte), Integer))
+        Me.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.label1.Enabled = False
+        Me.label1.Font = New System.Drawing.Font("Showcard Gothic", 26.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.label1.Location = New System.Drawing.Point(151, 40)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(391, 67)
+        Me.label1.TabIndex = 0
+        Me.label1.Text = "Hello World"
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Button1.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(232, 218)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(215, 56)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Click"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(151, 144)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(360, 31)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "km"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(742, 450)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ ShivaniHello/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' label1.Visible = False
+        label1.Text = TextBox1.Text
+        Dim str As String = TextBox1.Text
+        Dim msgBoxResult = MsgBox(Prompt:=str.Reverse)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.DialogResult = MessageBox.Show("Do you want ot exit", "Quit", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Error, MessageBoxDefaultButton.Button2)
+        If (Me.DialogResult = DialogResult.Yes) Then
+            Me.Close()
+        End If
+    End Sub
+End Class
